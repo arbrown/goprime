a74c35b072536f385429172358761cadbc3c258d
Drew Brown <drew.brown@multiling.com>
2013-05-31 13:22:07 -0600

    It works

--- prime.go
+++ prime.go
@@ -2,45 +2,69 @@ package main
 
 import (
 		"fmt"
+		"os"
+		"strconv"
 		)
 
 func main() {
+	var primesToPrint int
+	if len(os.Args) > 1 {
+		primesToPrint, _ = strconv.Atoi(os.Args[1])
+	}else {
+		primesToPrint = 5
+	}	
+	
 	candidateChan := make(chan int)
 	printChan := make(chan int)
-	max := 25
-	go produceCandidates(max,candidateChan)
+	go produceCandidates(candidateChan)
 	go (filter(5, candidateChan, printChan))()
 	
+	// 2,3,5 are trivial primes...
 	
-	for i:=0;i<6;i++{
+	fmt.Printf("2\n3\n5\n")
+	for i:=3; i<primesToPrint; i++{
 		fmt.Println(<-printChan)
 	}
 	
 }
 
-func produceCandidates(max int, ch chan int) {
+func produceCandidates(ch chan int) {
 	gap := 4
 	cand := 7
-	for i:=0;i<max;i++ {
+	for {
 		ch <- cand
 		cand += gap
 		gap = 6 - gap
 	}
+	close(ch)
 }
 
 func filter(seed int, in chan int, out chan int) func() {
 	test := seed
 	gap := 4
+	myout := out
 	return func () {
-		for {
-			cand := <- in
-			if cand >= test {
-				test += gap * test
-				gap = 6 - gap
+	//fmt.Printf("Starting new goroutine with %d\n", seed)
+		for cand:= range in {
+			if cand == test {
+			//fmt.Printf("%d == %d. And is a multiple of %d\n", cand, test, seed)
 			} else {
-				out <- cand
+				myout <- cand
+				// If we just found a prime, launch a new goroutine with a new filter
+				if  myout == out {
+					//fmt.Printf("** Just printed prime (%d).  Launching new goroutine.\n", cand)
+					ch := make(chan int)
+					myout = ch
+					go (filter(cand, ch, out))()
+				}
+			}
+			if cand > test {
+				//fmt.Printf("%d is greater than the test (%d).  Adding %d * %d\n", cand, test, seed, gap)
+				test += gap * seed
+				gap = 6 - gap
 			}
 		}
+		close(out)
 	}
 }
 
